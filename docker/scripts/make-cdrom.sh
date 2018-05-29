a2199bf205addeff87d45cf8e984a3ad2f998f09
RJ Bergeron <rbergero@gmail.com>
2018-05-29 11:45:24 -0400

    update iso scripts to match a little closer for each

--- docker/scripts/make-cdrom.sh
+++ docker/scripts/make-cdrom.sh
@@ -2,9 +2,14 @@
 
 set -eux
 
-[ -d /output ] || {
-  echo "script requires /output volume mount for saving contents" 1>&2
+[ -d /workdir ] || {
+  echo "script requires /workdir volume mount for saving contents" 1>&2
   exit 1
+}
+
+xorriso_image_graft="-graft-points /images=/images"
+[ -d /workdir/images ] && {
+  xorriso_image_graft="-graft-points /images=/workdir/images"
 }
 
 xorriso --report_about HINT -as xorrisofs -U -A xe_installer -V xe_installer -volset xe_installer -r -rational-rock -o /tmp/installercore.iso \
@@ -14,6 +19,7 @@ xorriso --report_about HINT -as xorrisofs -U -A xe_installer -V xe_installer -vo
   -graft-points "/opt=/opt" -graft-points "/root=/root" -graft-points "/sbin=/sbin" -graft-points "/scripts=/scripts" \
   -graft-points "/srv=/srv" -graft-points "/usr=/usr" \
   -graft-points "/etc=/cdroot/etc" \
+  ${xorriso_image_graft} \
   -partition_cyl_align off -partition_offset 0 -apm-block-size 2048 \
   -b isolinux/isolinux.bin -c boot/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table \
   -isohybrid-mbr "/isolinux/isohdpfx.bin" --protective-msdos-label \
@@ -25,4 +31,4 @@ xorriso --report_about HINT -as xorrisofs -U -A xe_installer -V xe_installer -vo
 
 isohybrid /tmp/installercore.iso
 
-mv /tmp/installercore.iso /output
+mv /tmp/installercore.iso /workdir
--- make-iso.sh
+++ make-iso.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-docker run --name newfs build/xenial-installer true
+docker run --name newfs --entrypoint true build/xenial-installer
 
 scratch=$(mktemp -d /var/tmp/newfs.XXXXXX)
 isolinux=$(mktemp -d /var/tmp/isolinux.XXXXXX)
@@ -19,9 +19,12 @@ docker export newfs | tar xf - -C "${scratch}" '--exclude=dev/*' '--exclude=var/
 
 docker rm newfs
 
-ln -sf "../proc/self/mounts" "${scratch}/etc/mtab"
-
 cp -R "${scratch}/boot" "${isolinux}/boot"
+
+xorriso_image_graft="-graft-points /images=${scratch}/images"
+if [ -d "$(pwd)/images" ] ; then
+  xorriso_image_graft="-graft-points /images=$(pwd)/images"
+fi
 
 for k in "${isolinux}/boot"/vmlinuz* "${isolinux}/boot"/initrd.img* ; do
   d="${k##*/}"
@@ -30,12 +33,18 @@ done
 
 xorriso --report_about HINT -as xorrisofs -U -A xe_installer -V xe_installer -volset xe_installer -r -rational-rock -o installercore.iso \
   -graft-points "/isolinux=${isolinux}" \
+  -graft-points "/EFI=${scratch}/EFI" -graft-points "/bin=${scratch}/bin" -graft-points "/boot=${scratch}/boot" -graft-points "/etc=${scratch}/etc" \
+  -graft-points "/lib=${scratch}/lib" -graft-points "/lib64=${scratch}/lib64" -graft-points "/media=${scratch}/media" -graft-points "/mnt=${scratch}/mnt" \
+  -graft-points "/opt=${scratch}/opt" -graft-points "/root=${scratch}/root" -graft-points "/sbin=${scratch}/sbin" -graft-points "/scripts=${scratch}/scripts" \
+  -graft-points "/srv=${scratch}/srv" -graft-points "/usr=${scratch}/usr" \
+  -graft-points "/etc=${scratch}/cdroot/etc" \
+  ${xorriso_image_graft} \
   -partition_cyl_align off -partition_offset 0 -apm-block-size 2048 -iso_mbr_part_type 0x00 \
   -b isolinux/isolinux.bin -c boot/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table \
   -isohybrid-mbr "${scratch}/isolinux/isohdpfx.bin" --protective-msdos-label \
   -eltorito-alt-boot -e /boot/efiboot.img -no-emul-boot -isohybrid-gpt-basdat \
   -eltorito-alt-boot -e /boot/macboot.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus \
-  "${scratch}" -- \
+  "${scratch}/cdroot" -- \
   -zisofs level=9:block_size=128k set_filter_r --zisofs /bin /etc /lib /opt /root /sbin /scripts /usr -- \
   -chmodi u+s /usr/bin/sudo --
 
