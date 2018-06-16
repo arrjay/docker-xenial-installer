3ebe2cacacaa2f9ef4a5753a3d81149f09a783c8
RJ Bergeron <rbergeron@palantir.com>
2018-06-15 17:53:17 -0700

    reorder copies to more easily supplant isolinux bits needed

--- make-iso.sh
+++ make-iso.sh
@@ -9,8 +9,6 @@ isolinux=$(mktemp -d /var/tmp/isolinux.XXXXXX)
 
 yum -y install syslinux xorriso
 
-cp /usr/share/syslinux/*.c32 /usr/share/syslinux/isolinux.bin /usr/share/syslinux/isohd*.bin "${isolinux}"
-
 docker export newfs | tar xf - -C "${scratch}" '--exclude=dev/*' '--exclude=var/*' '--exclude=tmp/*' '--exclude=etc/ssh/*' \
   '--exclude=home/*' \
   '--exclude=usr/lib/locale' '--exclude=usr/share/locale' '--exclude=lib/gconv' '--exclude=lib64/gconv' \
@@ -21,17 +19,14 @@ docker export newfs | tar xf - -C "${scratch}" '--exclude=dev/*' '--exclude=var/
 
 docker rm newfs
 
-cp -R "${scratch}/boot" "${isolinux}/boot"
+cp -R "${scratch}/isolinux/" "${isolinux}/"
+
+cp /usr/share/syslinux/*.c32 /usr/share/syslinux/isolinux.bin /usr/share/syslinux/isohd*.bin "${isolinux}"
 
 xorriso_image_graft="-graft-points /images=${scratch}/images"
 if [ -d "$(pwd)/images" ] ; then
   xorriso_image_graft="-graft-points /images=$(pwd)/images"
 fi
-
-for k in "${isolinux}/boot"/vmlinuz* "${isolinux}/boot"/initrd.img* ; do
-  d="${k##*/}"
-  ln "${k}" "${isolinux}/${d}"
-done
 
 xorriso --report_about HINT -as xorrisofs -U -A xe_installer -V xe_installer -volset xe_installer -r -rational-rock -o installercore.iso \
   -graft-points "/isolinux=${isolinux}" \
