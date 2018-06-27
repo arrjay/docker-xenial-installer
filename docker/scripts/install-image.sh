9631536940dd42a292d72a2398261d52b4bb1f0f
RJ Bergeron <rbergero@gmail.com>
2018-06-27 13:53:40 -0400

    update install-image to use a new finalization layer

--- docker/scripts/install-image.sh
+++ docker/scripts/install-image.sh
@@ -11,9 +11,5 @@ mount -o bind /sys/ /mnt/sysimage/sys/
 
 chroot /mnt/sysimage /bin/run-parts /scripts/grub-config
 chroot /mnt/sysimage /bin/run-parts /scripts/dracut-config
-
-: >   /mnt/sysimage/etc/machine-id
-rm -f /mnt/sysimage/var/lib/dbus/machine-id
-
-rm -f /mnt/sysimage/etc/resolv.conf
-ln -s /run/resolvconf/resolv.conf /mnt/sysimage/etc/resolv.conf
+chroot /mnt/sysimage passwd root
+chroot /mnt/sysimage /bin/run-parts /scripts/final-config
