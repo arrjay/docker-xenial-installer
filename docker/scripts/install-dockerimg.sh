ca8cabb2ddbba29cd076810ba6945ae8f2ecaf58
RJ Bergeron <rbergeron@palantir.com>
2018-06-27 21:34:52 -0700

    add script to pull docker image and install that

---
+++ docker/scripts/install-dockerimg.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+HOST=docker.io
+IMAGE=redjays/xenial-bootable
+TAG=latest
+DIRECTORY=/mnt/sysimage
+QUIET="false"
+
+OPTIND=1
+while getopts d:h:i:t:q _opt ; do
+  case ${_opt} in
+    d)
+      DIRECTORY="${OPTARG}"
+      ;;
+    h)
+      HOST="${OPTARG}"
+      ;;
+    i)
+      IMAGE="${OPTARG}"
+      ;;
+    t)
+      TAG="${OPTARG}"
+      ;;
+    q)
+      QUIET="true"
+      ;;
+  esac
+done
+
+set -e
+
+if [ "${QUIET}" == "false" ] ; then
+  # attempt to determine tty or pipe
+  if [ -t 0 ] ; then
+    curl_layer_flags="-#"
+  else
+    curl_layer_flags="-sS"
+  fi
+else
+  curl_layer_flags="-sS"
+fi
+
+echo "retriving manifest https://${HOST}/v2/${IMAGE}/manifests/${TAG}"
+for l in $(curl -sS -u 'anonymous:' "https://${HOST}/v2/${IMAGE}/manifests/${TAG}" | jq ."fsLayers"[].blobSum|sed -e s/\"//g|tac) ; do
+  echo "retrieving layer ${l}"
+  mkdir -p "${DIRECTORY}/tmp/${l}"
+  curl "${curl_layer_flags}" -u 'anonymous:' "https://${HOST}/v2/${IMAGE}/blobs/$l" | tar xpz --numeric-owner -C "${DIRECTORY}/tmp/${l}"
+  find "${DIRECTORY}/tmp/${l}" -name .wh.\* | while read -r path; do
+    echo "recreating ${path}"
+    rm "$path" && touch "$path"
+  done
+  rsync -aH "${DIRECTORY}/tmp/${l}/" "${DIRECTORY}/"
+  rm -rf "${DIRECTORY}/tmp/${l}"
+  find "${DIRECTORY}" -name .wh.\* | while read -r path; do
+    basename="${path##*/}"
+    dirname="${path%${basename}}"
+    basename="${basename#.wh.}"
+    echo "whiteout file in layer - removing ${dirname}${basename}"
+    rm -rf "$path" "${dirname}${basename}"
+  done
+done
+
+rsync -av /tmp/overlay/ /mnt/sysimage/
+
+mount -o bind /dev/ /mnt/sysimage/dev/
+mount -o bind /proc/ /mnt/sysimage/proc/
+mount -o bind /sys/ /mnt/sysimage/sys/
+
+chroot /mnt/sysimage /bin/run-parts /scripts/grub-config
+chroot /mnt/sysimage /bin/run-parts /scripts/dracut-config
+chroot /mnt/sysimage passwd root
+chroot /mnt/sysimage /bin/run-parts /scripts/final-config
