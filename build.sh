a5da41385cf96f21899ffb670af1a8da615a206b
RJ Bergeron <rbergero@gmail.com>
2018-07-05 13:33:34 -0400

    NOPUSH support during builds

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-docker pull "${DOCKER_SINK}/xenial-bootable"
+[ -z "${NOPUSH}" ] && docker pull "${DOCKER_SINK}/xenial-bootable"
 docker tag  "${DOCKER_SINK}/xenial-bootable" "xenial-bootable"
 
 set -u
